5baf200de0bba3a11cb9d2af5268bbad6b090649
ddaniel83 <101056315+ddaniel83@users.noreply.github.com>
2023-01-16 19:04:06 +0100

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -1,1 +1,17 @@
+import base64
 
+
+def encryption(string):
+    print(f'String before encoding: {string}')
+    encoded_string = string.encode('ascii')
+
+    encoded_bytes = base64.b64encode(encoded_string)
+    base64_string = encoded_bytes.decode("ascii")
+    print(f'Encoded string: {base64_string}')
+
+    decoded_string_bytes = base64.b64decode(base64_string)
+    decoded_string = decoded_string_bytes.decode('ascii')
+    print(f'Decoded string: {decoded_string}')
+
+
+encryption('Test')
